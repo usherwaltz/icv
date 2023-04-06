4b9e43fafa790a5ec59b38b55c645cacc4a79688
Nikola Jovic <usherwalce@gmail.com>
2023-04-06 21:30:42 +0200

    Removed asset

--- lib/assets/assets.dart
+++ lib/assets/assets.dart
@@ -2,5 +2,4 @@
 class Assets {
   static const String nikola = 'assets/images/nikola.png';
   static const String flutterLogo = 'assets/images/flutter_logo.svg';
-  static const String user = 'assets/images/user.png';
 }
